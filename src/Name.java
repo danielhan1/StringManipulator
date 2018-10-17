27af74d8ac3f2a1c0bbe82b4320e1d3ff229bcb3
Daniel <42978531+danielhan1@users.noreply.github.com>
2018-10-17 14:28:07 -0400

    Add files via upload

---
+++ src/Name.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author dhan19
+ *
+ */
+public class Name
+{
+			public static void main(String[] args)
+			{
+				
+				StringManipulator strm = new StringManipulator();
+				
+				System.out.print(strm.noVowels("daneiil"));
+			}
+}
--- src/StringManipulator.java
+++ src/StringManipulator.java
@@ -1,4 +1,33 @@
+/**
+ * 
+ * @author dhan19
+ *
+ */
+public class StringManipulator 
 
-public class StringManipulator {
-
+{
+				public String noVowels(String str)
+				{
+					String name = "";
+					for(int i = 0; i < (str.length()); i++)
+					{	
+					
+								if(str.charAt(i)=='a' || name.charAt(i)=='e' || name.charAt(i)=='i' || name.charAt(i)=='o' || name.charAt(i)=='u')
+								{
+									name += "*";
+								}
+								else 
+								{
+									name += str.charAt(i);
+									
+								}
+								
+					}
+					return name;
+				}
 }
+	
+					
+							
+				
+
